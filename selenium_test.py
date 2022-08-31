49ed21a3b6334132858fbab43d029e76e3fae94b
Thien Nguyen <104371084+ThienNguyenThanh@users.noreply.github.com>
2022-08-31 15:46:27 +0700

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -8,9 +8,7 @@ chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).in
 
 chrome_options = Options()
 options = [
-    "--headless",
     "--disable-gpu",
-    "--window-size=1920,1200",
     "--ignore-certificate-errors",
     "--disable-extensions",
     "--no-sandbox",
