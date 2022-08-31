1b7b57fdc72729fc5765c50388b4ee38c5101509
Thien Nguyen <104371084+ThienNguyenThanh@users.noreply.github.com>
2022-08-31 15:03:22 +0700

    Update selenium_test.py

--- selenium_test.py
+++ selenium_test.py
@@ -1,11 +1,25 @@
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.core.utils import ChromeType
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
-driver = webdriver.Chrome(service=chrome_service)
+chrome_options = Options()
+options = [
+    "--headless",
+    "--disable-gpu",
+    "--window-size=1920,1200",
+    "--ignore-certificate-errors",
+    "--disable-extensions",
+    "--no-sandbox",
+    "--disable-dev-shm-usage"
+]
+for option in options:
+    chrome_options.add_argument(option)
+
+driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 driver.get('http://54.152.49.220/')
 driver.quit()
