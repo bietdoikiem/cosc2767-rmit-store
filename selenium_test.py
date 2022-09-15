6ae02c9414c5d9606ebee19d54d22b9607e95a61
bietdoikiem <minhlucky2408@gmail.com>
2022-09-15 10:56:49 +0700

    fix: fix another wrong IP addresses

--- selenium_test.py
+++ selenium_test.py
@@ -1,26 +1,23 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.utils import ChromeType
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
-chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+chrome_service = Service(
+    ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
 
 chrome_options = Options()
 options = [
-    "--headless",
-    "--disable-gpu",
-    "--ignore-certificate-errors",
-    "--disable-extensions",
-    "--no-sandbox",
-    "--disable-dev-shm-usage"
+    "--headless", "--disable-gpu", "--ignore-certificate-errors",
+    "--disable-extensions", "--no-sandbox", "--disable-dev-shm-usage"
 ]
 for option in options:
-    chrome_options.add_argument(option)
+  chrome_options.add_argument(option)
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
 #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
-driver.get('http://54.85.101.87/')
+driver.get('http://35.153.46.184/')
 driver.quit()
